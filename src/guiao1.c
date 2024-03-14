4312c05beb63ccf9390d59a206089019824d076f
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-14 14:44:32 +0000

    Started Project

---
+++ src/guiao1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+#include <wchar.h>
+
+// char cardvalues[14] = {'1', '2', '3', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'};
+
+// int highestCard(char *card1, char *card2)
+// {
+
+// }
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    printf("%s %s %s %s %s %s\n", "\xF0\x9F\x82\xA2", "\xF0\x9F\x82\xA6", "\xF0\x9F\x82\xA8", "\xF0\x9F\x82\xA9", "\xF0\x9F\x82\xAA", "\xF0\x9F\x82\xAB");
+
+}
