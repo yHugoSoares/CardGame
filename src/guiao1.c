9ea818071924cb53de739219e378773c9aad1289
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-18 17:06:40 +0000

    Small changes

--- src/guiao1.c
+++ src/guiao1.c
@@ -3,20 +3,28 @@
 #include <string.h>
 #include <math.h>
 #include <locale.h>
-#include <wchar.h>
+#include <uchar.h>
 
 // char cardvalues[14] = {'1', '2', '3', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'};
 
-// int highestCard(char *card1, char *card2)
-// {
-
-// }
-
+wchar_t highestCard(wchar_t cards[])
+{
+    wchar_t cardCompare  = cards[0];
+    for (int i = 0; cards[i] != '\0' ; i++)
+    {
+        if (cardCompare < cards[i])
+        {
+            cardCompare = cards[i];
+        }
+    }   
+    return cardCompare;
+}
 
 
 int main()
 {
     setlocale(LC_ALL, "");
-    printf("%s %s %s %s %s %s\n", "\xF0\x9F\x82\xA2", "\xF0\x9F\x82\xA6", "\xF0\x9F\x82\xA8", "\xF0\x9F\x82\xA9", "\xF0\x9F\x82\xAA", "\xF0\x9F\x82\xAB");
+    wchar_t cards[100] = {0x1F0B6, 0x1F0C7, 0x1F0D3, 0x1F0AA};
+    printf("A maior carta é: %lc\n", highestCard(cards));
 
 }
