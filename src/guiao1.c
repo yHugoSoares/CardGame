f44bb502f14b0a98ea35e8268338b52e1e05fe72
Hugo Soares <145548037+yHugoSoares@users.noreply.github.com>
2024-03-23 18:16:17 +0000

    Update guiao1.c

--- src/guiao1.c
+++ src/guiao1.c
@@ -3,19 +3,21 @@
 #include <string.h>
 #include <math.h>
 #include <locale.h>
-#include <uchar.h>
+#include <wchar.h>
 
-// char cardvalues[14] = {'1', '2', '3', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'};
+// char cardvalues[14] = {'A', '2', '3', '3', '4', '5', '6', '7', '8', '9', '10', 'V', 'D', 'R'};
+
+// int highestCard(char *card1, char *card2)
+// {
+
+// }
 
 wchar_t highestCard(wchar_t cards[])
 {
     wchar_t cardCompare  = cards[0];
     for (int i = 0; cards[i] != '\0' ; i++)
     {
-        if (cardCompare < cards[i])
-        {
-            cardCompare = cards[i];
-        }
+        if (cardCompare < cards[i]) cardCompare = cards[i];
     }   
     return cardCompare;
 }
@@ -30,18 +32,42 @@ int countCards(wchar_t cards[])
     return count;
 }
 
+int conjunto(wchar_t cards[])
+{
+
+    for (int i = 0; cards[i] != '\0'; i++)
+    {
+        return 0;
+    }
+
+    return 0;
+}
 
 int main()
 {
-    int n = 0;
-    if (scanf("%d", &n) == 1);
-    for (int i = 0; i <= n; i++)
-    {
-        setlocale(LC_ALL, "");
-        wchar_t cards[100] = {0x1F0B6, 0x1F0C7, 0x1F0D3, 0x1F0AA};
-        printf("A maior carta é: %lc\n", highestCard(cards));
-    }
-    
+    int l;
+    wchar_t cards[100];
+    if (scanf("%d", &l) != 1) return 0;
     
 
+    for (int i = 0; i < l; i++){
+        
+        if (fgets(cards, 100, stdin) == 0) return 0;
+        
+        if (conjunto(cards) == 1)
+            printf("conjunto com %d cartas onde a carta mais alta é %lc\n", countCards(cards), highestCard(cards));
+        else if (seq(cards) == 1)
+            printf("sequência com %d cartas onde a carta mais alta é %lc\n", countCards(cards), highestCard(cards));
+        else if (dseq(cards) == 1)
+            printf("dupla sequência com %d cartas onde a carta mais alta é %lc\n", countCards(cards)/2, highestCard(cards));
+        else
+            printf("Nada!");
+
+    }
+
+    setlocale(LC_ALL, "");
+    wchar_t cards[100] = {0x1F0B6, 0x1F0C7, 0x1F0D3, 0x1F0AA};
+    printf("A maior carta é: %lc\n", highestCard(cards));
+
+    return 0;
 }
