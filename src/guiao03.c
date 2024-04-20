54436aa04f4d02fd6437e30488ee2fbad9c86b59
Hugo Soares <Hugoferreirasoares@gmail.com>
2024-04-20 20:25:02 +0100

    DONE

--- src/guiao03.c
+++ src/guiao03.c
@@ -5,48 +5,204 @@
 #include <locale.h>
 #include <wchar.h>
 #include <assert.h>
-#include "functions.h"
 
-// KING VALUE (mod 16) == 14
+// CÃ³digo dos guiÃµes passados
 
-// verifica se uma carta está presente na mão do jogador
-int pertenceMao(wchar_t jogadaAtual[], wchar_t maoCartas[])
+wchar_t maiorCarta(wchar_t cartas[], int numCartas) // encontra a maior carta
 {
-    long unsigned int r = 0;
-    for(int j = 0; jogadaAtual[j] != 0; j++)
-        for (int i = 0; maoCartas[i] != '\0'; i++)
-            r += comparaCartas(jogadaAtual[j], maoCartas[i]);
+    wchar_t atualMaiorCarta = cartas[0];
 
-    return (r == wcslen(jogadaAtual));
+    // ordem dos naipes: espadas < copas < ouros < paus
+
+    for (int i = 1; i < numCartas ; i++) // itera o conjunto de cartas
+    {
+        if (cartas[i]%16 > atualMaiorCarta%16) 
+        // se a carta for maior
+            atualMaiorCarta = cartas[i];
+        else if (cartas[i]%16 == atualMaiorCarta%16 && cartas[i] > atualMaiorCarta) 
+        // se carta for igual e naipe maior
+            atualMaiorCarta = cartas[i];
+    }
+
+    return atualMaiorCarta;
 }
 
-void retirarJogada(wchar_t jogadaAtual[], wchar_t maoCartas[])
+int conjunto(wchar_t cartas[], int numCartas) // verifica se Ã© um conjunto
 {
-    for (int i = 0; jogadaAtual[i] != '\0'; i++)
+    for (int i = 0; i < numCartas; i++)
     {
-        for (int j = 0; maoCartas[j] != '\0'; j++)
+        if (cartas[0]%16 != cartas[i]%16) return 0; // se as cartas forem diferentes
+        for (int j = 0; j < numCartas; j++)
         {
-            if (maoCartas[j] == jogadaAtual[i]) 
-            {
-                for (int k = j; maoCartas[k] != '\0'; k++) maoCartas[k] = maoCartas[k + 1];
-            }
-            break; 
+            if (cartas[i] == cartas[j] && i != j) return 0; // se houver cartas iguais
+        }
+    }    
+    return 1;
+}
+
+int checkConsecutivo(wchar_t cartas[], int numCartas, int mult2, wchar_t cartaAtual, int *indexConsecutivo)
+{
+    for (int j = 0; j < numCartas*mult2; j++)
+    {
+        if ((cartaAtual%16)+1 == cartas[j]%16) // verifica se tem consecutivo
+        {
+            *indexConsecutivo = j;
+            return 1;
         }
     }
+
+    return 0;
+}
+
+int checkImagem(wchar_t cartas[], int numCartas, int mult2, wchar_t cartaAtual)
+{
+    for (int j = 0; j < numCartas*mult2; j++)
+    {
+        if (cartaAtual%16 == cartas[j]%16 && cartaAtual != cartas[j]) // verifica se tem imagem
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int seq(wchar_t cartas[], int numCartas, int mult2) // verifica se Ã© uma sequÃªncia
+{
+
+    if (numCartas <= 2) return 0; // se nÃ£o tem cartas suficientes para formar uma sequÃªncia
+
+    wchar_t menorCarta = cartas[0];
+
+    for (int i = 0; i < numCartas*mult2; i++) // encontra a menor carta
+        if (cartas[i]%16 < menorCarta%16) menorCarta = cartas[i];
+    
+    wchar_t cartaAtual = menorCarta;
+
+    for (int i = 0; i < numCartas;i++) // itera o conjunto de cartas por ordem crescente
+    {
+        int indexConsecutivo = 0;
+        int temConsecutivo = 0;
+        int temImagem = 1;
+
+        if (i == numCartas-1) temConsecutivo = 1; // a Ãºltima carta tem consecutivo por defeito
+        else temConsecutivo = checkConsecutivo(cartas,numCartas,mult2,cartaAtual,&indexConsecutivo); // teste se tem consecutivo
+
+        if (mult2 == 2) temImagem = checkImagem(cartas,numCartas,mult2,cartaAtual); // testa se tem imagem (caso dupla sequencia)
+
+        if (temConsecutivo && temImagem) cartaAtual = cartas[indexConsecutivo];
+        if (temConsecutivo == 0 || temImagem == 0) return 0; // se nÃ£o tiver uma carta consecutiva ou imagem
+    }
+
+    return 1;
+}
+
+int comparaCartas(wchar_t carta1, wchar_t carta2) // compara duas cartas (retorna 1 quando a primeira Ã© maior)
+{
+    if (carta1%16 > carta2%16 || (carta1%16 == carta2%16 && carta1 > carta2))
+        return 1;
+    else
+        return 0;
+}
+
+void isort(wchar_t cartas[], int numCartas) // implementaÃ§Ã£o do insertion sort para ordenar as cartas de uma sequÃªncia
+{	
+    int i, j;
+    wchar_t aux;
+    for (i = 0; i < numCartas; i++) 
+    {
+        aux = cartas[i];	
+        for (j = i; j > 0 && comparaCartas(cartas[j-1],aux); j--)
+            cartas[j] = cartas[j-1];	
+        cartas[j] = aux;	
+    }	
+}
+
+// Novo CÃ³digo
+
+// verifica se as cartas da jogada atual estÃ£o presentes na mÃ£o do jogador
+int pertenceMao(wchar_t jogadaAtual[], wchar_t maoCartas[])
+{
+    int tamanhoMao = wcslen(maoCartas);
+    int tamanhoJogada = wcslen(jogadaAtual);
+    int r = 0;
+    for(int i = 0; i < tamanhoJogada; i++)
+        for (int j = 0; j < tamanhoMao; j++)
+            if (jogadaAtual[i] == maoCartas[j])
+                r++;
+
+    if (r == tamanhoJogada) return 1;
+    else return 0;
+}
+
+// retira da mÃ£o do jogador as cartas presentes na jogada atual
+void retirarJogada(wchar_t jogadaAtual[], wchar_t maoCartas[])
+{
+    int tamanhoMao = wcslen(maoCartas);
+    int tamanhoJogada = wcslen(jogadaAtual);
+    wchar_t temp[tamanhoMao];
+    int tempIndex = 0;
+
+    for(int i = 0; i < tamanhoMao; i++)
+    {
+        int naoEJogado = 1;
+        for (int j = 0; j < tamanhoJogada; j++)
+            if (maoCartas[i] == jogadaAtual[j])
+                naoEJogado = 0;
+        
+        if (naoEJogado)
+        {
+            temp[tempIndex] = maoCartas[i];
+            tempIndex++;
+        }
+    }
+
+    for(int i = 0; i < tempIndex; i++)
+        maoCartas[i] = temp[i];
+}
+
+int casosEspeciais(wchar_t jogadaAtual[], wchar_t jogadaAnterior[])
+{
+    int tamanhoJogada = wcslen(jogadaAtual);
+    int tamanhoAnterior = wcslen(jogadaAnterior);
+
+    int tudoReis = 1;
+    for (int i = 0; i < tamanhoAnterior; i++)
+        if (jogadaAnterior[i] % 16 != 14) tudoReis = 0;
+    
+    if (tudoReis)
+    {
+        if (tamanhoAnterior == 1 && conjunto(jogadaAtual, tamanhoJogada) && tamanhoJogada == 4)
+            return 1;
+        else if (tamanhoAnterior == 1 && seq(jogadaAtual, tamanhoJogada/2, 2) && tamanhoJogada/2 == 3)
+            return 1;
+        else if (tamanhoAnterior == 2 && seq(jogadaAtual, tamanhoJogada/2, 2) && tamanhoJogada/2 == 4)
+            return 1;
+        else if (tamanhoAnterior == 3 && seq(jogadaAtual, tamanhoJogada/2, 2) && tamanhoJogada/2 == 5)
+            return 1;
+    }
+
+    return 0;
 }
 
 
-// verifica se a jogada atual é do mesmo tipo, de tamanho igual e de valor superior à jogada anterior
+// verifica se a jogada atual Ã© do mesmo tipo, de tamanho igual e de valor superior Ã  jogada anterior
 int jogadaSuperior(wchar_t jogadaAtual[], wchar_t jogadaAnterior[])
 {
+    if (casosEspeciais(jogadaAtual, jogadaAnterior)) return 1;
+    
+    int tamanhoJogada = wcslen(jogadaAtual);
+    int tamanhoAnterior = wcslen(jogadaAnterior);
     int tipoIgual = 0;
     int tamanhoIgual = 0;
     int valorSuperior = 0;
 
-    if ( (conjunto(jogadaAtual, wcslen(jogadaAtual)) && conjunto(jogadaAnterior, wcslen(jogadaAtual))) ||
-         (seq(jogadaAtual, wcslen(jogadaAtual), 1)   && seq(jogadaAnterior, wcslen(jogadaAtual), 1))   ||
-         (seq(jogadaAtual, wcslen(jogadaAtual)/2, 2) && seq(jogadaAnterior, wcslen(jogadaAtual)/2, 2))    )
-            tipoIgual = 1;
+    if (conjunto(jogadaAtual,tamanhoJogada) && conjunto(jogadaAnterior, tamanhoAnterior))
+        tipoIgual = 1;
+    else if (seq(jogadaAtual, tamanhoJogada, 1) && seq(jogadaAnterior, tamanhoAnterior, 1))
+        tipoIgual = 1;
+    else if (seq(jogadaAtual, tamanhoJogada/2, 2) && seq(jogadaAnterior, tamanhoAnterior/2, 2))
+        tipoIgual = 1;
 
     if (wcslen(jogadaAtual) == wcslen(jogadaAnterior))
             tamanhoIgual = 1;
@@ -58,54 +214,62 @@ int jogadaSuperior(wchar_t jogadaAtual[], wchar_t jogadaAnterior[])
     else return 0;
 }
 
-
-// combinacaoValida que testa se a jogada recebida como argumento é conjunto, seq ou dupla seq
+// escrever combinacaoValida que testa se a jogada recebida como argumento Ã© conjunto, seq ou dupla seq
 int combinacaoValida(wchar_t jogadaAtual[])
 {
-    // Verifica se é um conjunto
-    if (conjunto(jogadaAtual, wcslen(jogadaAtual)))
-        return 0;
-    // Verifica se é uma sequência
-    if (seq(jogadaAtual, wcslen(jogadaAtual), 1))
-        return 0;
-    // Verifica se é uma dupla sequência
-    if (seq(jogadaAtual, wcslen(jogadaAtual) / 2, 2))
-        return 0;
-
-    return 1;
+    int tamanhoJogada = wcslen(jogadaAtual);
+    if  ((conjunto(jogadaAtual, tamanhoJogada)) ||
+         (seq(jogadaAtual, tamanhoJogada, 1))   ||
+         (seq(jogadaAtual, tamanhoJogada/2, 2))   )
+            return 1;
+    else return 0;
 }
 
+// verifica se a jogada Ã© vÃ¡lida
+int jogadaValida(wchar_t jogadasAnteriores[][1000], int numJogadasAnteriores, wchar_t jogadaAtual[])
+{    
+    if (numJogadasAnteriores == 0)
+        return combinacaoValida(jogadaAtual);
 
-int jogadaValida(wchar_t jogadasAnteriores[][100], int numJogadasAnteriores, wchar_t jogadaAtual[])
-{
-    for (int i = numJogadasAnteriores-1; i > numJogadasAnteriores-4 && i > 0; i--)
-        // verifica se a jogada anterior é "PASSO" e se a jogada atual é uma combinação válida
-        if  (jogadasAnteriores[i][0] == 'P' && combinacaoValida(jogadaAtual)) continue;
-        // verifica se a jogada atual é do mesmo tipo, de tamanho igual e de valor superior à jogada anterior
-        else if (jogadaSuperior(jogadaAtual, jogadasAnteriores[i])) break;
-        // se nenhuma das anteriores se verificar a jogada não é válida
+    for (int i = numJogadasAnteriores-1; i > numJogadasAnteriores-4 && i >= 0; i--)
+    {
+        // verifica se a jogada anterior Ã© "PASSO" e se a jogada atual Ã© uma combinaÃ§Ã£o vÃ¡lida
+        if  (jogadasAnteriores[i][0] == 'P' && combinacaoValida(jogadaAtual))
+            continue;
+        // verifica se a jogada atual Ã© do mesmo tipo, de tamanho igual e de valor superior Ã  jogada anterior
+        else if (jogadaSuperior(jogadaAtual, jogadasAnteriores[i]))
+            break;
+        // se nenhuma das anteriores se verificar a jogada nÃ£o Ã© vÃ¡lida
         else return 0;
+    }
     
     return 1;
 }
 
-void teste(wchar_t maoCartas[], wchar_t jogadasAnteriores[][100], int numJogadasAnteriores, wchar_t jogadaAtual[])
+// verifica se a jogada Ã© vÃ¡lida e modifica a mÃ£o do jogador
+int aplicaJogada(wchar_t maoCartas[], wchar_t jogadasAnteriores[][1000], int numJogadasAnteriores, wchar_t jogadaAtual[])
 {
-    // Verifica se as cartas jogadas pertencem à mão do jogador e são uma jogada válida
+
+    // verifica se as cartas jogadas pertencem Ã  mÃ£o do jogador e sÃ£o uma jogada vÃ¡lida
     if (pertenceMao(jogadaAtual, maoCartas) && jogadaValida(jogadasAnteriores, numJogadasAnteriores, jogadaAtual))
     {
         retirarJogada(jogadaAtual, maoCartas);
-        isort(maoCartas, wcslen(maoCartas));
+        return 1;
     }
+    
+    return 0;
 }
 
-void escrevecarta(wchar_t maoCartas[], int numCartas) // escreve as cartas com espacos entre elas
+
+void imprimeMao(wchar_t maoCartas[], int tamanhoMao)
 {
-    for (int i = 0; i < numCartas; i++)
-    {
-        wprintf(L"%lc ", maoCartas[i]);
-    }
-    wprintf(L"\n");
+    if (tamanhoMao == 0) wprintf(L"\n");
+    else for(int i = 0; i < tamanhoMao; i++)
+         {
+             if (i == tamanhoMao - 1)
+                 wprintf(L"%lc\n", maoCartas[i]);
+             else wprintf(L"%lc ", maoCartas[i]);
+         }
 }
 
 int main()
@@ -113,40 +277,54 @@ int main()
     setlocale(LC_CTYPE, "C.UTF-8");
     
     int testes, numJogadasAnteriores;
-    wchar_t jogadaAtual[100];
-    wchar_t maoCartas[100];
+    wchar_t jogadaAtual[1000];
+    wchar_t maoCartas[1000];
 
-    // lê o número de testes que vão ser lidos
-    assert(wscanf(L"%d", &testes) == 1); 
+    // lÃª o nÃºmero de testes que vÃ£o ser lidos
+    assert(wscanf(L"%d", &testes) == 1);
 
     // itera para cada teste
     for (int i = 0; i < testes; i++)
     {
-        // lê e guarda o número de jogadas passadas que vão ser lidas
+        // lÃª e guarda o nÃºmero de jogadas passadas que vÃ£o ser lidas
         assert(wscanf(L"%d", &numJogadasAnteriores) == 1);
 
         // array de jogadas passadas
-        wchar_t jogadasAnteriores[numJogadasAnteriores][100];
+        wchar_t jogadasAnteriores[numJogadasAnteriores][1000];
 
-        // lê e guarda as cartas do jogador
-        assert(wscanf(L"%100ls", maoCartas) != 0);
+        // lÃª e guarda as cartas do jogador
+        assert(wscanf(L"%1000ls", maoCartas) != 0);
+        int tamanhoMao = wcslen(maoCartas);
         
-        // lê e guarda as jogadas anteriores (linha a linha)
+        // lÃª e guarda as jogadas anteriores (linha a linha)
         for (int j = 0; j < numJogadasAnteriores; j++)
-            assert(wscanf(L"%100ls", &jogadasAnteriores[j][0]) != 0);
+            assert(wscanf(L"%1000ls", &jogadasAnteriores[j][0]) != 0);
         
-        // lê e guarda a jogada atual do jogador
-        assert(wscanf(L"%100ls", jogadaAtual) != 0);
+        // lÃª e guarda a jogada atual do jogador
+        assert(wscanf(L"%1000ls", jogadaAtual) != 0);
 
         // print do teste atual
         wprintf(L"Teste %d\n", i+1);
 
-        // loop que imprime a mao com espaços
-        teste(maoCartas, jogadasAnteriores, numJogadasAnteriores, jogadaAtual);
-        escrevecarta(maoCartas, wcslen(maoCartas));
+        // se o jogador passar a vez
+        if (wcscmp(jogadaAtual, L"PASSO") == 0)
+        {
+            isort(maoCartas, tamanhoMao);
+            imprimeMao(maoCartas, tamanhoMao);
+            continue;
+        }
 
-        //proximo teste
-        printf("\n");
+        // verifica se a jogada Ã© vÃ¡lida e modifica a mÃ£o do jogador
+        int jogadaAplicada = aplicaJogada(maoCartas, jogadasAnteriores, numJogadasAnteriores, jogadaAtual);
+        
+        // determina o nÃºmero de cartas na mÃ£o do jogador depois da jogada
+        if (jogadaAplicada) tamanhoMao -= wcslen(jogadaAtual);
+
+        // ordena as cartas
+        isort(maoCartas, tamanhoMao);
+
+        // imprime a mÃ£o do jogador
+        imprimeMao(maoCartas, tamanhoMao);
         
     }
     
